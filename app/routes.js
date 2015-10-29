492410b8c301934680a32f95ee70245ce7df4ea7
Rishin Doshi <rishdosh@umich.edu>
2015-10-29 14:50:09 -0400

    clean up

--- app/routes.js
+++ app/routes.js
@@ -7,71 +7,38 @@ module.exports = function(app){
 	});
 
 	app.get('/artist', function(req, res){
-		console.log("IN /ARTIST")
+		console.log("IN /ARTIST ROUTE")
 		var search_artist_name = req.query.artist_name;
 		var popular_tracks = [];
 
+		var artist_req_options = {
+			url: 'https://api.spotify.com/v1/search',
+			qs: {q: search_artist_name, type: 'artist'},
+			method: 'GET'
+		};
+		var tracks_req_options = {
+			url: 'https://api.spotify.com/v1/artists/',
+			qs: {country: 'US'},
+			method: 'GET'	
+		};
+
 		if(search_artist_name){
-			request({
-			    url: 'https://api.spotify.com/v1/artists/5K4W6rqBFWDnAN6FQUkS6x/top-tracks', //URL to hit
-			    qs: {country: 'US'}, //Query string data
-			    method: 'GET',
-			}, function(error, response, body){
-			    if(error) {
-			        console.log(error);
-			    } else {
-			    	var tracks = JSON.parse(body).tracks;
+			request(artist_req_options, function(error, response, body){
+				var data = JSON.parse(body);
+				var search_artist_id = data.artists.items[0].id;
+				tracks_req_options.url += (search_artist_id + '/top-tracks');
+				request(tracks_req_options, function(error, response, body){
+					var tracks = JSON.parse(body).tracks;
 			     	var popular_tracks = [];
-					for(var i=0; i<tracks.length; ++i){
-						popular_tracks.push(tracks[i].name);
-					}
-				}	
+			     	tracks.forEach(function(track){
+			     		popular_tracks.push(track.name);
+			     	});
+					console.log(popular_tracks);
+				});
+
 			});
 		}
 
-		// if(search_artist_name){
-		// 	var options = {
-		// 		hostname: "api.spotify.com",
-		// 		path: '/v1/search?q=Kendrick&type=artist',
-		// 		method: 'GET'
-		// 	}
-		// 	console.log(options.hostname + options.path);
-		// 	var search_artist_id = null;
-		// 	var req = https.request(options, function(res){
-		// 		res.on('data', function(data){
-		// 			data_json = JSON.parse(data);
-		// 			search_artist_id = data_json.artists.items[0].id;
-		// 			// console.log(data_json.artists.items[0].id);
-
-		// 			var options2 = {
-		// 				hostname: "api.spotify.com",
-		// 				path: '/v1/artists/5K4W6rqBFWDnAN6FQUkS6x/top-tracks?country=US',
-		// 				method: 'GET'
-		// 			};
-		// 			console.log("second request");
-		// 			var req = https.request(options2, function(res){
-		// 				var body = '';
-		// 				res.on('data', function(data2){
-		// 					body += data2;
-		// 				});
-		// 				res.on('end', function(){
-		// 					var data_json = JSON.parse(body);
-		// 					var tracks = data_json.tracks;
-		// 					for(var i=0; i<tracks.length; ++i){
-		// 						popular_tracks.push(tracks[i].name);
-		// 					}
-		// 					console.log(popular_tracks);
-		// 				})
-
-		// 			});
-		// 			req.end();
-		// 		});
-		// 	})
-		// 	req.end();
-		// 	req.on('error', function(error){
-		// 		console.log("ERROR: " + error);
-		// 	});
-		// }
-
+		
 	});
 }
